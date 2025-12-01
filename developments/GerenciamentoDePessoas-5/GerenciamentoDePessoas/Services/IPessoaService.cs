35b1bdfcd8924068d08f69bd993e5f3b612cc8e1
Daniel Dantas <contatodanieldantasdev@gmail.com>
2025-12-01 01:07:44 -0300

    feat: adição do arquivo IPessoaService.cs

---
+++ developments/GerenciamentoDePessoas-5/GerenciamentoDePessoas/Services/IPessoaService.cs
@@ -0,0 +1,15 @@
+﻿using GerenciamentoDePessoas.Models;
+
+namespace GerenciamentoDePessoas.Services
+{
+    public interface IPessoaService
+    {
+        Task<List<Pessoa>> BuscarTodos();
+        Task<int> BuscarTotal();
+        Task<List<string>> BuscarPessoasNome(string termo);
+        Task<Pessoa> Criar(Pessoa pessoa);
+        Task<Pessoa> BuscarPorId(int id);
+        Task<Pessoa> Editar(Pessoa pessoa);
+        Task Apagar(int id);
+    }
+}
