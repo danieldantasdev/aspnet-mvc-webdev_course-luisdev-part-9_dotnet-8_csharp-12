5ad839b94b01000e276b03c6d7c85582a1484a83
Daniel Dantas <contatodanieldantasdev@gmail.com>
2025-12-01 01:07:42 -0300

    feat: adição do arquivo Program.cs

---
+++ developments/GerenciamentoDePessoas-5/GerenciamentoDePessoas/Program.cs
@@ -0,0 +1,45 @@
+using GerenciamentoDePessoas.Data;
+using GerenciamentoDePessoas.Repository;
+using GerenciamentoDePessoas.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace GerenciamentoDePessoas
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContext<GerenciamentoDePessoasContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("GerenciamentoDePessoasContext")));
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.AddScoped<IPessoaService, PessoaService>();
+            builder.Services.AddScoped<IPessoaRepository, PessoaRepository>(); 
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
